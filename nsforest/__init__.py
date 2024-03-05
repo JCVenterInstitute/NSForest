913e89e5f37607ec12e80f670fff3db4a279920b
Beverly Peng <42978187+BeverlyPeng@users.noreply.github.com>
2024-03-05 14:59:40 -0800

    Delete nsforest directory

--- nsforest/__init__.py
+++
@@ -1,3 +0,0 @@
-from nsforest.nsforest import myRandomForest,myDecisionTreeEvaluation,NSForest,preprocessing_medians,get_medians
-__all__ = ['myRandomForest','myDecisionTreeEvaluation','NSForest','preprocessing_medians','get_medians']
-__version__ = "4.0.0"
