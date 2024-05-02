fb528174e0e4ac8ad8175cc0798a6af9265f7bd0
Beverly Peng <bpeng0507@gmail.com>
2024-05-02 12:01:35 -0700

    Update __init__.py

--- nsforest/__init__.py
+++ nsforest/__init__.py
@@ -4,7 +4,8 @@ from . import preprocessing as pp
 from . import nsforesting
 from . import evaluating as ev
 from . import plotting as pl
+from utils import str_to_list
 
-__all__ = ["ns", "pp", "nsforesting", "ev", "pl"]
+__all__ = ["ns", "pp", "nsforesting", "ev", "pl", "str_to_list"]
 
 __version__ = "4.0.0"
