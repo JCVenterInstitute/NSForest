d8c177c3b5cb30ffbff6ff46ae2cb402a5dd2332
Beverly Peng <bpeng0507@gmail.com>
2024-10-02 14:27:42 -0700

    Update myrandomforest.py

--- nsforest/nsforesting/myrandomforest.py
+++ nsforest/nsforesting/myrandomforest.py
@@ -37,7 +37,7 @@ def myRandomForest(adata, df_dummies, cl, n_trees, n_jobs, n_top_genes, binary_d
     ## pre-select genes based on gene_selection criterium
     ind_genes_selected = binary_dummies.loc[cl] == 1
     x_train = x_train.loc[:,ind_genes_selected]   # subset x_train
-    print("\t", "Pre-selected", x_train.shape[1], "genes to feed into Random Forest.")
+    print(f"\tPre-selected {x_train.shape[1]} genes to feed into Random Forest.")
 
     rf_clf = RandomForestClassifier(n_estimators=n_trees, n_jobs=n_jobs, random_state=123456) #<===== criterion="gini", by default
     rf_clf.fit(x_train, y_train)
