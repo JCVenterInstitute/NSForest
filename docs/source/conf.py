4f9c07c4b25e02c19bf5c9ed6d84ab8dcaccc975
Beverly Peng <42978187+BeverlyPeng@users.noreply.github.com>
2025-03-21 14:16:44 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ import sys
 # sys.path.insert(0, os.path.abspath("../../nsforest/plotting"))
 
 # sys.path.insert(0, os.path.abspath(".."))
-# sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../.."))
 # sys.path.insert(0, os.path.abspath("../../.."))
 
 # -- Project information
