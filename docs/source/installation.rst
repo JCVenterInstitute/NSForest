5592dbd8c9c28b0c712358271e4399c3780898b6
Beverly Peng <bpeng0507@gmail.com>
2026-03-26 15:20:17 -0700

    updating installation notes

--- docs/source/installation.rst
+++ docs/source/installation.rst
@@ -1,27 +1,39 @@
 Installation
 ============
 
-To install NSForest via Github: 
+To install NS-Forest via Github: 
 
 .. code-block:: console
 
-   $ git clone https://github.com/JCVenterInstitute/NSForest.git
-   $ cd NSForest
+   git clone https://github.com/JCVenterInstitute/NSForest.git
+   cd NSForest
    
-Now we create the environment with all the dependencies required to run NSForest, including the ability to run a jupyter lab notebook version of the tutorial. This was successfully run on a Macbook pro, Xtools installed, Apple M3 pro chip, Sonoma 14.6 operating system.
+Now we create the environment with all the dependencies required to run NS-Forest, including the ability to run a jupyter lab notebook version of the tutorial. This was successfully run on a Macbook pro, Xtools installed, Apple M3 pro chip, Sonoma 14.6 operating system.
 
-Additionally, emacs is installed, to faciliate editing.   Feel free to inspect the yaml file, nsforest.yml.   Once the environment is created then we activate the environment. 
-
-.. code-block:: console
-
-   $ conda env create -f environment.yml
-   $ conda activate environment
-   $ cd tutorials
-
-Once the environment is activated.  We need to install the NSForest package.
+Additionally, emacs is installed to faciliate editing. Once the environment is created then we activate the environment. 
 
 .. code-block:: console
 
-   $ pip install .
+   conda env create -f environment.yml
+   conda activate environment
 
-You should be able to "import nsforest" and run the tutorial.ipynb. 
+There are 2 ways to run NS-Forest: Jupyter Notebook and command line
+
+Jupyter Notebook: docs/source/tutorial_nsforesting.ipynb
+
+.. code-block:: console
+   
+   import sys
+   import os
+   code_folder = "location/of/NSForest/folder"
+   sys.path.insert(0, os.path.abspath(code_folder))
+   import nsforest as ns
+   from nsforest import utils
+
+Command Line: nsforest/__main__.py.
+
+.. code-block:: console
+
+   pip install .
+   python3 nsforest
+
--- nsforest/__main__.py
+++ nsforest/__main__.py
@@ -6,13 +6,10 @@ sys.path.insert(0, os.path.abspath("./nsforest/nsforesting"))
 import numpy as np
 import pandas as pd
 import scanpy as sc
+import anndata as ad
 import matplotlib.pyplot as plt
 import nsforest as ns
 from nsforest import utils
-from nsforest import preprocessing as pp
-from nsforest import nsforesting
-from nsforest import evaluating
-from nsforest import plotting as pl
 
 """\
 If running locally: python3 nsforest
@@ -24,46 +21,36 @@ def main():
     # Preparing kwargs
     args = utils._parse_args(sys.argv[1:], description=__doc__)
 
-    file = "../demo_data/adata_layer1.h5ad" 
+    data_folder = "demo_data/"
+    file = data_folder + "adata_layer1.h5ad"
     adata = sc.read_h5ad(file)
-    adata.var_names = adata.var["feature_name"]
 
     cluster_header = "cluster"
-    medians_header = "medians_" + cluster_header
-    binary_scores_header = "binary_scores_" + cluster_header
+    output_folder = "outputs_layer1/"
 
     #### Preprocessing ####
-    ns.pp.dendrogram(adata, cluster_header) 
-    ns.pp.prep_medians(adata, cluster_header, use_mean = False, positive_genes_only = True) 
-    ns.pp.prep_binary_scores(adata, cluster_header, medians_header) 
+    ns.pp.dendrogram(adata, cluster_header, save = True, output_folder = output_folder, outputfilename_suffix = cluster_header)
+
+    # Only run these 2 lines if running ns.nsforesting.NSForest
+    adata = ns.pp.prep_medians(adata, cluster_header)
+    adata = ns.pp.prep_binary_scores(adata, cluster_header)
 
     filename = file.replace(".h5ad", "_preprocessed.h5ad")
     print(f"Saving new anndata object as...\n{filename}")
     adata.write_h5ad(filename)
 
-    # Start here if you already preprocessed the anndata
-    adata = sc.read_h5ad(file.replace(".h5ad", "_preprocessed.h5ad"))
-
-    outputfilename = cluster_header
-    output_folder = "../outputs_layer1/"
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-        print(f"Creating new directory...\n{output_folder}")
-
     #### Running NSForest ####
-    # Adding this line to actually subset to positive genes if running NSForest
-    adata = ns.pp.prep_medians(adata, cluster_header, use_mean = False, positive_genes_only = True) 
+    outputfilename_prefix = cluster_header
     if args.c: 
-        nsforesting.NSForest(adata, cluster_header, medians_header, binary_scores_header, cluster_list = [args.c], output_folder = output_folder, outputfilename = outputfilename)
+        results = ns.nsforesting.NSForest(adata, cluster_header, cluster_list = [args.c], save_supplementary = True, save = True, output_folder = output_folder, outputfilename_prefix = outputfilename_prefix)
     else: 
-        results = nsforesting.NSForest(adata, cluster_header, medians_header, binary_scores_header, output_folder = output_folder, outputfilename = outputfilename)
+        results = ns.nsforesting.NSForest(adata, cluster_header, save_supplementary = True, save = True, output_folder = output_folder, outputfilename_prefix = outputfilename_prefix)
 
-    #### Evaluating markers ####
-    outputfilename = "markers"
-    markers = pd.read_csv("../outputs_layer1/cluster_results.csv")
-    markers_dict = utils.prepare_markers(markers, "clusterName", "NSForest_markers")
-    adata = sc.read_h5ad(file.replace(".h5ad", "_preprocessed.h5ad"))
-    results = evaluating.DecisionTree(adata, cluster_header, medians_header, markers_dict, combinations = False, output_folder = output_folder, outputfilename = outputfilename)
+    # #### Evaluating markers ####
+    # outputfilename_prefix = "marker_eval"
+    # markers = pd.read_csv("outputs_layer1/cluster_results.csv")
+    # markers_dict = utils.prepare_markers(markers, "clusterName", "NSForest_markers")
+    # results = ns.ev.DecisionTree(adata, cluster_header, markers_dict, combinations = False, use_mean = False, save_supplementary = True, output_folder = output_folder, outputfilename_prefix = outputfilename_prefix)
 
     # Loading results csv
     # if parallelizing, remember to combine the separate output csv's into a single results csv before plotting
@@ -71,13 +58,6 @@ def main():
     # results["markers"] = utils.str_to_list(results["markers"])
 
     #### Plotting ####
-    ns.pl.boxplot_fscore(results, True, output_folder, outputfilename)
-    ns.pl.boxplot_ppv(results, True, output_folder, outputfilename)
-    ns.pl.boxplot_ontarget(results, True, output_folder, outputfilename)
-    ns.pl.scatter_w_clusterSize_fscore(results, True, output_folder, outputfilename)
-    ns.pl.scatter_w_clusterSize_ppv(results, True, output_folder, outputfilename)
-    ns.pl.scatter_w_clusterSize_ontarget(results, True, output_folder, outputfilename)
-
     # If you want to specify dendrogram order
     dendrogram = list(adata.uns["dendrogram_" + cluster_header]["categories_ordered"])
     if dendrogram: # https://stackoverflow.com/questions/23482668/sorting-by-a-custom-list-in-pandas
@@ -85,12 +65,18 @@ def main():
         results["clusterName"] = results["clusterName"].cat.set_categories(dendrogram)
         results = results.sort_values("clusterName")
     
-    markers = []
-    for val in results["NSForest_markers"]: # markers NSForest_markers binary_markers
-        markers.extend(val)
+    markers_dict = utils.prepare_markers(results, "clusterName", "NSForest_markers")
     
-    ns.pl.dotplot(adata, markers, cluster_header, dendrogram, True, output_folder, outputfilename)
-    ns.pl.stackedviolin(adata, markers, cluster_header, dendrogram, True, output_folder, outputfilename)
-    ns.pl.matrixplot(adata, markers, cluster_header, True, True, output_folder, outputfilename)
+    # # Scanpy plots
+    # ns.pl.dotplot(adata, markers_dict, cluster_header, dendrogram = dendrogram, save = True, output_folder = output_folder, outputfilename_suffix = outputfilename_prefix)
+    # ns.pl.stackedviolin(adata, markers_dict, cluster_header, dendrogram = dendrogram, save = True, output_folder = output_folder, outputfilename_suffix = outputfilename_prefix)
+    # ns.pl.matrixplot(adata, markers_dict, cluster_header, dendrogram = dendrogram, save = True, output_folder = output_folder, outputfilename_suffix = outputfilename_prefix)
+
+    # # Plotly plots
+    # ns.pl.boxplot(results, ["f_score", "precision", "recall", "onTarget"], save = True, output_folder = output_folder, outputfilename_prefix = outputfilename_prefix)
+    # ns.pl.scatter_w_clusterSize(results, "f_score", save = True, output_folder = output_folder, outputfilename_prefix = outputfilename_prefix)
+    # ns.pl.scatter_w_clusterSize(results, "precision", save = True, output_folder = output_folder, outputfilename_prefix = outputfilename_prefix)
+    # ns.pl.scatter_w_clusterSize(results, "recall", save = True, output_folder = output_folder, outputfilename_prefix = outputfilename_prefix)
+    # ns.pl.scatter_w_clusterSize(results, "onTarget", save = True, output_folder = output_folder, outputfilename_prefix = outputfilename_prefix)
 
 main()
